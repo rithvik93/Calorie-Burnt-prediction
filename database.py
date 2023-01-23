1c020c4f4943451d56474f046cb746f61661ccc0
rithvik93 <91080873+rithvik93@users.noreply.github.com>
2023-01-24 02:29:43 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 22 19:41:34 2023
+
+@author: RITHVIK
+"""
+
+from deta import Deta
+DETA_KEY="d0ldf89w_NiE9WtM9mgv3h9yTpDAqfFeGBeW9y66o"
+# Initialize with a project key
+deta = Deta(DETA_KEY)
+# This is how to create/connect a database
+db = deta.Base("Users_data")
+
+def insert_data(age,gender,height,weight,duration,heart_rate,temparature,result):
+    return db.put({"age": age, "gender": gender,"Height":height,"Weight":weight,"Duration":duration,
+        "Heart_rate":heart_rate,"Temparature":temparature,"Result":result})
+
