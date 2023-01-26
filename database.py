f7e4e26802692fdececd03666417f4ae5ed79579
rithvik93 <91080873+rithvik93@users.noreply.github.com>
2023-01-26 17:57:05 +0530

    Update database.py

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ Created on Sun Jan 22 19:41:34 2023
 """
 
 from deta import Deta
+DETA_KEY=st.secrets["DETA_KEY"] 
 # Initialize with a project key
 deta = Deta(DETA_KEY)
 # This is how to create/connect a database
